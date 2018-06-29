5da5d51833254817035b013fc49d68e969f8c0c6
G.Provost <grace.g.provost@gmail.com>
2018-06-29 09:31:22 -0700

    getting further

--- de-duplicate/deDuplicate.js
+++ de-duplicate/deDuplicate.js
@@ -1,47 +1,54 @@
 
 export const compare = (current, next) => {
   //newest date trumps
-  
-  
-  
-  //otherwise furthest down in list
+  return new Date(current.entryDate) > new Date(next.entryDate) 
+    ? current
+    : next; 
+};
+
+export const filter = (set) => {
+
+  for(let key in set) {
+    set[key] = set[key].reduce((prev, curr) => compare(prev, curr));
+  }
+
+  return set;
 };
 
 export const deDuplicate = (leads) => {
 
-  //define a set for the de-duped records
+  const list = leads.leads;
 
-  const deDuped = {};
+  //define maps for the records
+  const ids = {};
+  const emails = {};
 
-  //de-stringify the JSON
-  const leadsWithDups = JSON.parse(leads);
-  //loop over records
-  for(let record of leadsWithDups) {
+  //loop over records and place into respective maps
+  for(let record of list) {
 
     const { _id, email } = record;
-    //check email and _id for existence and place into new set if not
+    //check email and _id for existence and place into new array within maps (to maintain order)
 
-    if(deDuped[email]) {
-    //if they do exist, begin other checks
-    
-      deDuped[email] = compare(deDuped[email], record);
+    emails[email] 
+      ? emails[email].push(record) 
+      : emails[email] = [record];
 
-    } 
-    else deDuped[email] = record;
-
-    if(deDuped[_id]) {
-    //if they do exist, begin other checks
-      deDuped[_id] = compare(deDuped[_id], record);
-
-    } 
-    else deDuped[_id] = record;
+    ids[_id]
+      ? ids[_id].push(record)
+      : ids[_id] = [record];
   }
+
+  //filter over each to remove dups
+
+  const filteredIds = Object.values(filter(ids));
+  const filteredEmails = Object.values(filter(emails));
+
+  return {...filteredIds, ...filteredEmails };
   
   //provide a log of changes that include:
   //source record, output record
   //individual field changes (from, to)
   //output is in same format as input JSON
 
-  return deDuped;
   
 };
--- de-duplicate/deDuplicate.test.js
+++ de-duplicate/deDuplicate.test.js
@@ -1,44 +1,46 @@
 
-import { deDuplicate, compare } from './deDuplicate';
+import { deDuplicate, compare, filter } from './deDuplicate';
+import leads from './leads.json';
 
-describe('deDuplicate tests', () => {
-  //record1 and record2 have same email
-  const record1 = {
-    _id: 'jkj238238jdsnfsj23',
-    email: 'foo@bar.com',
-    firstName:  'John',
-    lastName: 'Smith',
-    address: '123 Street St',
-    entryDate: '2014-05-07T17:30:20+00:00'
-  };
+//record1 and record2 have same email
+const record1 = {
+  _id: 'jkj238238jdsnfsj23',
+  email: 'foo@bar.com',
+  firstName:  'John',
+  lastName: 'Smith',
+  address: '123 Street St',
+  entryDate: '2014-05-07T17:30:20+00:00'
+};
 
-  const record2 = {
-    _id: 'sel045238jdsnfsj23',
-    email: 'foo@bar.com',
-    firstName:  'John',
-    lastName: 'Smith',
-    address: '123 Street St',
-    entryDate: '2014-05-07T17:32:20+00:00'
-  };
+const record2 = {
+  _id: 'sel045238jdsnfsj23',
+  email: 'foo@bar.com',
+  firstName:  'John',
+  lastName: 'Smith',
+  address: '123 Street St',
+  entryDate: '2014-05-07T17:32:20+00:00'
+};
 
-  //record3 and record4 have same date and email
-  const record3 = {
-    _id: 'vug789238jdsnfsj23',
-    email: 'foo1@bar.com',
-    firstName:  'Blake',
-    lastName: 'Douglas',
-    address: '123 Reach St',
-    entryDate: '2014-05-07T17:33:20+00:00'
-  };
+//record3 and record4 have same date and email
+const record3 = {
+  _id: 'vug789238jdsnfsj23',
+  email: 'foo1@bar.com',
+  firstName:  'Blake',
+  lastName: 'Douglas',
+  address: '123 Reach St',
+  entryDate: '2014-05-07T17:33:20+00:00'
+};
 
-  const record4 = {
-    _id: 'wuj08238jdsnfsj23',
-    email: 'foo1@bar.com',
-    firstName:  'Micah',
-    lastName: 'Valmer',
-    address: '123 Street St',
-    entryDate: '2014-05-07T17:33:20+00:00'
-  };
+const record4 = {
+  _id: 'wuj08238jdsnfsj23',
+  email: 'foo1@bar.com',
+  firstName:  'Micah',
+  lastName: 'Valmer',
+  address: '123 Street St',
+  entryDate: '2014-05-07T17:33:20+00:00'
+};
+
+describe('compare tests', () => {
 
   it('returns a record that is newer in date', () => {
     const result = compare(record1, record2);
@@ -46,5 +48,36 @@ describe('deDuplicate tests', () => {
     expect(result).toBe(record2);
   });
 
+  it('returns a record that is next if they are equal in date', () => {
+    const result = compare(record3, record4);
+
+    expect(result).toBe(record4);
+  });
+
 }); 
+
+describe('filter tests', () => {
+
+  const set = { 'foo@bar.com': 
+  [{'_id': 'jkj238238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:30:20+00:00', 'firstName': 'John', 'lastName': 'Smith'}, {'_id': 'sel045238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:32:20+00:00', 'firstName': 'John', 'lastName': 'Smith'}, {'_id': 'wuj08238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:33:20+00:00', 'firstName': 'Micah', 'lastName': 'Valmer'}, {'_id': 'qest38238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com','entryDate': '2014-05-07T17:32:20+00:00', 'firstName': 'John', 'lastName': 'Smith'}]
+  };
+
+  const filteredSet = {'foo@bar.com': {'_id': 'wuj08238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:33:20+00:00', 'firstName': 'Micah', 'lastName': 'Valmer'}}
+
+  it('filters out all in set except for newest / most recent', () => {
+    const result = filter(set);
+
+    expect(result).toEqual(filteredSet);
+  });
+
+});
+
+describe('deDuplicate tests', () => {
+
+  it('returns a deduped list', () => {
+    const result = deDuplicate(leads);
+
+    expect(result).toBe({});
+  });
+});
 
