567988ee61ba37f7b3b3234701adedf00b05a7fd
G.Provost <grace.g.provost@gmail.com>
2018-06-29 13:03:33 -0700

    working on adding change logs

--- de-duplicate/deDuplicate.js
+++ de-duplicate/deDuplicate.js
@@ -1,18 +1,36 @@
+import leads from './leads.json';
 
 export const compare = (current, next) => {
-  //newest date trumps
+  //newest date trumps, else last in list
   return new Date(current.entryDate) > new Date(next.entryDate) 
     ? current
     : next; 
 };
 
-export const filter = (map) => {
+export const filter = (map, tracker) => {
+
+  const filtered = {};
 
   for(let key in map) {
-    map[key] = map[key].reduce((prev, curr) => compare(prev, curr));
-  }
 
-  return map;
+    //protect against filtering if there is only one record with that id / email
+    if(map[key].length === 1) {
+      filtered[key] = map[key][0];
+      continue;
+    } 
+    
+    //grab record prior to filter
+    tracker[key] = { from: map[key][0] };
+
+    //filter
+    filtered[key] = map[key].reduce((prev, curr) => compare(prev, curr));
+
+    tracker[key].to = filtered[key];
+
+  }
+  // console.log(tracker);
+
+  return { filtered, tracker };
 };
 
 export const mapMaker = (list, property) => {
@@ -29,22 +47,32 @@ export const mapMaker = (list, property) => {
 
 };
 
+export const consolidateChangeLogs = (logs) => {
+
+};
+
 export const deDuplicate = (leads) => {
 
   const list = leads.leads;
 
   //loop over records first with _ids to find dups
 
-  const filteredIds = Object.values(filter(mapMaker(list, '_id')));
+  const { filtered: filteredIds, tracker: trackerIds } = filter(mapMaker(list, '_id'), {});
 
-  //loop over these to filter out email dups
+  //loop over again to filter out email dups
 
-  return { 'leads': Object.values(filter(mapMaker(filteredIds, 'email'))) };
-  
+  const { filtered: finalOutput, tracker: finalTracker } = filter(mapMaker(Object.values(filteredIds), 'email'), trackerIds); 
+
   //provide a log of changes that include:
   //source record, output record
   //individual field changes (from, to)
   //output is in same format as input JSON
 
+  return { 
+    'source': list,
+    'output': Object.values(finalOutput),
+    'changes': finalTracker
+  };
   
+
 };
--- de-duplicate/deDuplicate.test.js
+++ de-duplicate/deDuplicate.test.js
@@ -56,30 +56,42 @@ describe('compare tests', () => {
 
 }); 
 
-describe('filter tests', () => {
+// describe('filter tests', () => {
 
-  const set = { 'foo@bar.com': 
-  [{'_id': 'jkj238238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:30:20+00:00', 'firstName': 'John', 'lastName': 'Smith'}, {'_id': 'sel045238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:32:20+00:00', 'firstName': 'John', 'lastName': 'Smith'}, {'_id': 'wuj08238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:33:20+00:00', 'firstName': 'Micah', 'lastName': 'Valmer'}, {'_id': 'qest38238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com','entryDate': '2014-05-07T17:32:20+00:00', 'firstName': 'John', 'lastName': 'Smith'}]
-  };
+//   const set = { 'foo@bar.com': 
+//   [{'_id': 'jkj238238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:30:20+00:00', 'firstName': 'John', 'lastName': 'Smith'}, {'_id': 'sel045238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:32:20+00:00', 'firstName': 'John', 'lastName': 'Smith'}, {'_id': 'wuj08238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:33:20+00:00', 'firstName': 'Micah', 'lastName': 'Valmer'}, {'_id': 'qest38238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com','entryDate': '2014-05-07T17:32:20+00:00', 'firstName': 'John', 'lastName': 'Smith'}]
+//   };
 
-  const filteredSet = {'foo@bar.com': {'_id': 'wuj08238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:33:20+00:00', 'firstName': 'Micah', 'lastName': 'Valmer'}}
+//   const filteredSet = {'foo@bar.com': {'_id': 'wuj08238jdsnfsj23', 'address': '123 Street St', 'email': 'foo@bar.com', 'entryDate': '2014-05-07T17:33:20+00:00', 'firstName': 'Micah', 'lastName': 'Valmer'}};
 
-  it('filters out all in set except for newest / most recent', () => {
-    const result = filter(set);
+//   const tracker = { 'foo@bar.com':       { from:          { _id: 'jkj238238jdsnfsj23',            address: '123 Street St',            email: 'foo@bar.com',            entryDate: '2014-05-07T17:30:20+00:00',            firstName: 'John',
+//   lastName: 'Smith' },
+// to:
+// { _id: 'wuj08238jdsnfsj23',
+//   address: '123 Street St',            email: 'foo@bar.com',
+//   entryDate: '2014-05-07T17:33:20+00:00',
+//   firstName: 'Micah',
+//   lastName: 'Valmer' } } };
 
-    expect(result).toEqual(filteredSet);
-  });
+//   it('filters out all in set except for newest / most recent and tracks changes when filtering', () => {
+//     const result = filter(set, {});
 
-});
+//     expect(result.filtered).toEqual(filteredSet);
+//     expect(result.tracker).toEqual(tracker);
+//   });
+
+// });
 
 describe('deDuplicate tests', () => {
 
-  const deDuped = {"leads": [{"_id": "jkj238238jdsnfsj23", "address": "888 Mayberry St", "email": "bill@bar.com", "entryDate": "2014-05-07T17:33:20+00:00", "firstName": "John", "lastName": "Smith"}, {"_id": "belr28238jdsnfsj23", "address": "123 Water St", "email": "mae@bar.com", "entryDate": "2014-05-07T17:33:20+00:00", "firstName": "Tallulah", "lastName": "Smith"}, {"_id": "wabaj238238jdsnfsj23", "address": "8803 Dark St", "email": "bog@bar.com", "entryDate": "2014-05-07T17:31:20+00:00", "firstName": "Fran", "lastName": "Jones"}, {"_id": "wuj08238jdsnfsj23", "address": "123 Street St", "email": "foo@bar.com", "entryDate": "2014-05-07T17:33:20+00:00", "firstName": "Micah", "lastName": "Valmer"}, {"_id": "vug789238jdsnfsj23", "address": "123 Reach St", "email": "foo1@bar.com", "entryDate": "2014-05-07T17:33:20+00:00", "firstName": "Blake", "lastName": "Douglas"}]};
+  const deDuped = 
+  [{"_id": "jkj238238jdsnfsj23", "address": "888 Mayberry St", "email": "bill@bar.com", "entryDate": "2014-05-07T17:33:20+00:00", "firstName": "John", "lastName": "Smith"}, {"_id": "belr28238jdsnfsj23", "address": "123 Water St", "email": "mae@bar.com", "entryDate": "2014-05-07T17:33:20+00:00", "firstName": "Tallulah", "lastName": "Smith"}, {"_id": "wabaj238238jdsnfsj23", "address": "8803 Dark St", "email": "bog@bar.com", "entryDate": "2014-05-07T17:31:20+00:00", "firstName": "Fran", "lastName": "Jones"}, {"_id": "wuj08238jdsnfsj23", "address": "123 Street St", "email": "foo@bar.com", "entryDate": "2014-05-07T17:33:20+00:00", "firstName": "Micah", "lastName": "Valmer"}, {"_id": "vug789238jdsnfsj23", "address": "123 Reach St", "email": "foo1@bar.com", "entryDate": "2014-05-07T17:33:20+00:00", "firstName": "Blake", "lastName": "Douglas"}];
 
   it('returns a deduped list', () => {
     const result = deDuplicate(leads);
 
-    expect(result).toEqual(deDuped);
+    expect(result['output']).toEqual(deDuped);
+    expect(result['changes']).toEqual({});
   });
 });
 
